706f82e2177ef43f6398e93ba4cdb49092439a09
tamakiii <tamaki.htm@gmail.com>
2018-05-24 22:19:26 +0900

    add browser option

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -8,7 +8,9 @@ var path = {
 gulp.task('build-postcss', function() {
   return gulp.src(path.post_src)
     .pipe(postcss([
-      require('autoprefixer')({}),
+      require('autoprefixer')({
+        browsers: ['last 2 versions', '> 2%']
+      }),
       require('cssnano')
     ]))
     .pipe(gulp.dest(path.css_dest));
