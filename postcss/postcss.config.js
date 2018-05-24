e0b2e8999eb1e1858afec14ce8c1096623c76851
tamakiii <tamaki.htm@gmail.com>
2018-05-24 23:30:55 +0900

    Revert "add postcss-use"
    This reverts commit 6ff99f0d8e48349fbbf31f0e07214beb5b3f41e5.

--- postcss/postcss.config.js
+++ postcss/postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
     require('postcss-import'),
     require('postcss-url'),
     require('postcss-cssnext'),
-    require('postcss-use'),
     require('stylelint'),
     require('cssnano'),
     require('postcss-browser-reporter')
