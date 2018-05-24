c216029301c0f53223e948f4c34a004c94af7690
tamakiii <tamaki.htm@gmail.com>
2018-05-25 00:08:14 +0900

    add more to postcss.config.js

--- hiq/postcss.config.js
+++ hiq/postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require('postcss-mixins'),
     require('postcss-custom-selectors'),
-    require('postcss-custom-media')
+    require('postcss-custom-media'),
+    require('postcss-easy-import'),
+    require('postcss-nesting'),
+    require('postcss-discard-comments')
   ]
 };
